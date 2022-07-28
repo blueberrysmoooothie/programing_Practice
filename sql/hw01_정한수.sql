fde1b51f9af71fb343c38650d59f2e08ec29b911
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2022-07-28 16:24:56 +0900

    Update hw01_정한수.sql

--- sql/hw01_정한수.sql
+++ sql/hw01_정한수.sql
@@ -1,4 +1,5 @@
 -- use testdb
+-- drop table world; 
 create table world
 	(
 	country VARCHAR(30),
@@ -11,23 +12,23 @@ create table world
 
 insert into world 
 (country, continent, area, population, capital, top_level_domain)
-values('Afghanistan', 'Asia', '652230', '25500100', 'Kabul', '.af'),
-('Algeria', 'Africa', '2381741', '38700000', 'Algiers', '.dz'),
-('New Zealand', 'Oceania', '270467', '4538520', 'Wellington', '.nz'),
-('Australia', 'Oceania', '7692024', '23545500', 'Canberra', '.au'),
-('Brazil', 'South America', '8515767', '202794000', 'Brasilia', '.br'),
-('China', 'Asia', '9596961', '1365370000', 'Beijing', '.cn'),
-('India', 'Asia', '3166414', '1246160000', 'New Delhi', '.in'),
-('Russia', 'Eurasia', '17125242', '146000000', 'Moscow', '.ru'),
-('France', 'Europe', '640679', '65906000', 'Paris', '.fr'),
-('Germany', 'Europe', '357114', '80716000', 'Berlin', '.de'),
-('Denmark', 'Europe', '43094', '5634437', 'Copenhagen', '.dk'),
-('Norway', 'Europe', '323802', '5124383', 'Oslo', '.no'),
-('Sweden', 'Europe', '450295', '9675885', 'Stockholm', '.se'),
-('South Korea', 'Asia', '100210', '50423955', 'Seoul', '.kr'),
-('Canada', 'North America', '9984670', '35427524', 'Ottawa', '.ca'),
-('United States', 'North America', '9826675', '318320000', 'Washington, D.C.', '.us'),
-('Armenia', 'Eurasia', '29743', '3017400', 'Yerevan', '.am');
+values('Afghanistan', 'Asia', 652230, 25500100, 'Kabul', '.af'),
+('Algeria', 'Africa', 2381741, 38700000, 'Algiers', '.dz'),
+('New Zealand', 'Oceania', 270467, 4538520, 'Wellington', '.nz'),
+('Australia', 'Oceania', 7692024, 23545500, 'Canberra', '.au'),
+('Brazil', 'South America', 8515767, 202794000, 'Brasilia', '.br'),
+('China', 'Asia', 9596961, 1365370000, 'Beijing', '.cn'),
+('India', 'Asia', 3166414, 1246160000, 'New Delhi', '.in'),
+('Russia', 'Eurasia', 17125242, 146000000, 'Moscow', '.ru'),
+('France', 'Europe', 640679, 65906000, 'Paris', '.fr'),
+('Germany', 'Europe', 357114, 80716000, 'Berlin', '.de'),
+('Denmark', 'Europe', 43094, 5634437, 'Copenhagen', '.dk'),
+('Norway', 'Europe', 323802, 5124383, 'Oslo', '.no'),
+('Sweden', 'Europe', 450295, 9675885, 'Stockholm', '.se'),
+('South Korea', 'Asia', 100210, 50423955, 'Seoul', '.kr'),
+('Canada', 'North America', 9984670, 35427524, 'Ottawa', '.ca'),
+('United States', 'North America', 9826675, 318320000, 'Washington, D.C.', '.us'),
+('Armenia', 'Eurasia', 29743, 3017400, 'Yerevan', '.am');
 
 -- 1. 전체 테이블 내용 출력
 select * from world;
