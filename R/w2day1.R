82e60bd6a0c1b4f513edad625b931dd885785585
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2022-08-10 19:52:04 +0900

    Update w2day1.R

--- R/w2day1.R
+++ R/w2day1.R
@@ -20,3 +20,72 @@ for ( i in seq(3,100, 3)){
 }
 cat(mean(mo), sd(mo),'\n')
 
+# 통계적 추정과 가설검정
+lower <- -1.96
+upper <- 0.5
+x<- seq(-5,5, length = 1000)
+y<-dnorm(x)
+plot(x,y,type = 'l', lwd = 2, col = 'tomato')
+# abline(0,0,col = 'tomato', lwd = 2)
+xlim <- x[lower <= x & x<= upper]
+ylim <- y[lower <= x & x<= upper]
+xlim <- c(xlim[1], xlim, tail(xlim,1))
+ylim <- c(0, ylim, 0)
+polygon(xlim, ylim, col = 'darkorange')
+
+
+# 이항분포 검정
+binom.test(x=60, n=100, p = 0.5)
+
+random_binom <- rbinom(1000, 100, 0.5)
+cat(mean(random_binom), sd(random_binom))
+# np, sqrt(npq)
+1-(2*pbinom(60,100,0.5, lower.tail = F))
+pnorm(60,50,5, lower.tail = F)
+
+
+# 정규성 검정 
+x<- rnorm(1000, 30, 7)
+shapiro.test(x)
+
+qqnorm(x, col = 'lightgray')
+qqline(x)
+
+
+# t 검정
+# - t분포 : 정규분포를 따르는 모집단으로 부터 추출한 표본의 확률분포
+x<- seq(-3,3, length = 1000)
+curve(dt(x, df = 30), min(x), max(x), lty = 1, lwd = 1, col = 'darkblue')
+curve(dt(x, df = 20), min(x), max(x), lty = 2, lwd = 1, col = 'lightgray', add = T)
+curve(dt(x, df = 10), min(x), max(x), lty = 3, lwd = 1, col = 'orange', add = T)
+curve(dt(x, df = 1), min(x), max(x), lty = 4, lwd = 1, col = 'steelblue', add = T)
+
+t<- (135 - 115)/(25/sqrt(20))
+pt(t,df = 19)
+1-pt(t,df = 19, lower.tail = F)*2 
+
+library(MASS)
+## 단일표본 평균검정
+result <-t.test(x=cats$Bwt, mu = 2.6)
+result
+
+result$estimate
+2*pt(result$statistic, result$parameter, lower.tail = F)
+
+result <-t.test(x=cats$Bwt, mu = 2.6, alternative = 'greater', conf.level = 0.99)
+result
+
+result$estimate
+pt(result$statistic, result$parameter, lower.tail = F)
+
+
+## 독립표본 평균검정
+result <- t.test(cats$Bwt ~cats$Sex)
+result
+
+
+## 대응표본 평균검정
+result <- t.test(extra ~ group, data = sleep, paired = T)
+result
+
+result$method
