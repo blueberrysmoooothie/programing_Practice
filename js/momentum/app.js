bee7acea501ca9edfe3cf7fa3ff6d6c29563c6fb
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-02-21 15:25:33 +0900

    Update app.js

--- js/momentum/app.js
+++ js/momentum/app.js
@@ -6,4 +6,31 @@ let flg = true
 let n = null;
 let m;
 flg = 'et'
-console.log(m);
+
+const l = [1,2,3,4,'d']
+l.push('new')
+
+// object
+const player = {
+    name : 'han',
+    age: 27,
+    height: 167
+}
+player.weight = 62
+player.weight += 1
+
+
+// function
+function hi(name){
+    console.log('hi '+name)
+}
+
+hi('has')
+
+const test_obj = {
+    name : 'test',
+    describe : function(){
+        console.log(test_obj.name)
+    }
+}
+test_obj.describe()
