ead2a305a4f09f08714c624acabe3f53e6b49bce
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-04-04 15:38:23 +0900

    Create test.py

---
+++ js/test.py
@@ -0,0 +1,12 @@
+def func(a, b, c):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+        c (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a + b + c
