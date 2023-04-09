e216ce9016120af2eb06339e5659c85bc112aedd
loading <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-04-09 16:49:57 +0900

    04/09
    remove class
    localStorage

--- js/momentum/app.js
+++ js/momentum/app.js
@@ -1,9 +1,10 @@
 const loginForm = document.getElementById('login-form')
-
 const loginInput = loginForm.querySelector("input")
-const loginButton = loginForm.querySelector("button")
-
-function loginButtonClicked() {
+const greeting = document.querySelector('#greeting')
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
+// const loginButton = loginForm.querySelector("button")
+// function loginButtonClicked() {
     // console.dir(loginButton)
     // let userId = loginInput.value
     // console.log(userId)
@@ -13,22 +14,28 @@ function loginButtonClicked() {
     // }else if(userId.length >= 15){
     //     alert("it's too long")
     // } else console.log('hi', userId);
+// }
+function showGreeting(){
+    const username = localStorage.getItem(USERNAME_KEY)
+    greeting.innerText = `hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    
 }
-
-function onLoginSubmit(arg1) {
-    arg1.preventDefault(); //default인 submit 방지
-    console.log(arg1)
-}
-
-loginButton.addEventListener("click", loginButtonClicked);
 
 // 자동으로 발생 이벤트 정보를 전달
-loginForm.addEventListener('submit', onLoginSubmit);
-
-function hrefClicked(event) {
-    event.preventDefault();
+function onLoginSubmit(arg1) {
+    arg1.preventDefault(); //default인 submit 방지
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
+    showGreeting()
 }
 
-
-const herf1 = document.querySelector("#href1");
-herf1.addEventListener('click', hrefClicked);
+// localStorage에 데이터가 있는지 확인
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else{
+    showGreeting()
+}
